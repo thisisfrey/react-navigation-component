c8c9dc849ecebc1ae1e8583f18d067abe8eda9ca
Antonia Frey <antonia.frey@devoteam.com>
2023-06-25 12:01:21 +0200

    Fix console router errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
