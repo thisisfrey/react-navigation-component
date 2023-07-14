2a49298ba5c565b8bdad34e1bfa41845a7e296f9
Antonia Frey <antonia.frey@devoteam.com>
2023-07-14 20:04:50 +0200

    Changed base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
   //base: "/react-navigation-component/",
   server: {
     port: 5000,
