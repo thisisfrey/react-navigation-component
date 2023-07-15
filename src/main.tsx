4c3ddc82889a06226cd217aa187de2d10ce773a5
Antonia Frey <antonia.frey@devoteam.com>
2023-07-15 09:44:26 +0200

    Added basename to BR

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter basename={"/react-navigation-component/"}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
